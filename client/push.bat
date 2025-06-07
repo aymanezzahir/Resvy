e3edf75626a9ad6f64a055d73cb6ae219b6afd9d
aymanezzahir <aymanefront2004@gmail.com>
2025-06-07 07:52:29 +0100

    import assets and app

--- client/push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git add -A
-set /p string=Give me your comment: 
-git commit -m "%string%"
-git push origin master
--- client/src/pages/notfound.tsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function Notfond(){
-    return (
-        <div className=" !font-bold text-red-600">"404 Not Found"</div>
-    )
-}
