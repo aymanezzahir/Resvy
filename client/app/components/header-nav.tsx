becee2bda0bbaa42315bdb1de18a227faad8a89d
aymanezzahir <aymanefront2004@gmail.com>
2025-07-02 12:09:06 +0100

    init home page

---
+++ client/app/components/header-nav.tsx
@@ -0,0 +1,64 @@
+
+import {Link, useLoaderData, useLocation, useNavigate, useParams} from "react-router";
+
+import {cn} from "~/../lib/util";
+
+const RootNavbar = () => {
+    const navigate = useNavigate();
+    const location = useLocation()
+    const params = useParams();
+    const user = useLoaderData();
+
+    const handleLogout = async () => {
+        
+        navigate('/sign-in')
+    }
+
+    console.log(user)
+
+    return (
+        <nav className={cn('w-full fixed z-50 glassmorphism')}>
+            <header className="root-nav wrapper">
+                <Link to='/' className="link-logo">
+                    <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]" />
+                    <h1>Tourvisto</h1>
+                </Link>
+
+             {user ?   <aside>
+                    {
+                    // user.status === 'admin'
+                    true
+                     && (
+                        <Link to="/dashboard" className={cn('text-base font-normal text-white', {"text-dark-100": location.pathname.startsWith('/travel')})}>
+                            Admin Panel
+                        </Link>
+                    )}
+
+                    <img src={user?.imageUrl || '/assets/images/david.wepb'} alt="user" referrerPolicy="no-referrer" />
+
+                    <button onClick={handleLogout} className="cursor-pointer">
+                        <img
+                            src="/assets/icons/logout.svg"
+                            alt="logout"
+                            className="size-6 rotate-180"
+                        />
+                    </button>
+                </aside> : 
+                
+                <aside>
+
+                    <Link to="/login" className={cn('text-base font-semibold text-primary-400', )}>
+                            Login
+                    </Link>
+
+                    <Link to="/signup" className={'text-base bg-primary-500 rounded-2xl  p-2 font-normal text-white'}>
+                            Sign Up
+                    </Link>
+                </aside>
+                
+                }
+            </header>
+        </nav>
+    )
+}
+export default RootNavbar
--- client/app/routes.ts
+++ client/app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index , layout, route} from "@react-router/dev/routes
 
 
 export default [
+    index("routes/home.tsx"),
     layout("routes/admin/layout.tsx" , [
         route("dashboard" , "routes/admin/dashboard.tsx"),
         route("all-users" , "routes/admin/all-user.tsx"),
